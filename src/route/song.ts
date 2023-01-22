fa07d4703eabf5375378b6e7798fe513d74b3e28
emilyalvae <emilisa23@gmail.com>
2023-01-22 14:13:38 -0500

    Agregando funcion listar cancion por id

--- src/route/song.ts
+++ src/route/song.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getSongs, createSongs } from "../controller/SongController";
+import { getSongs, createSongs, getSongById } from "../controller/SongController";
 
 const songRouter: Router = Router();
 
 songRouter.get("/", getSongs);
 songRouter.post("/", createSongs);
+songRouter.get("/:id", getSongById);
 
 export default songRouter;
