bf7abd199a9e040dfa9ea9377babcb961cf161a9
Ajcaam <alvarochancafe@gmail.com>
2023-01-25 00:24:10 -0500

    Correccion

--- src/route/song.ts
+++ src/route/song.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import {
   createSongs,
   getPublicSongs,
+  getPrivateSongs,
   getSongById,
+
 } from "../controller/SongController";
 import verifyTokenMidleware from "../midlewares/verifyTokenMidleware";
 
 const songRouter: Router = Router();
 
 songRouter.get("/", getPublicSongs);
-songRouter.get("/privates", verifyTokenMidleware, getPublicSongs);
+songRouter.get("/privates", verifyTokenMidleware, getPrivateSongs);
 songRouter.post("/", verifyTokenMidleware, createSongs);
 songRouter.get("/:id", verifyTokenMidleware, getSongById);
 
